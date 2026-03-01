0a92f4c03ab4fa203807057cc8cb7ef61ddd9b3e
shubham dubey <shubhamd1109@gmai.com>
2026-03-01 12:10:56 +0530

    Added co-relation id and enhanced logging feature.

--- order-service/src/main/java/com/ecom/order_service/event/OrderEventPublisher.java
+++ order-service/src/main/java/com/ecom/order_service/event/OrderEventPublisher.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import io.awspring.cloud.sns.core.SnsTemplate;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
@@ -24,9 +25,14 @@ public class OrderEventPublisher {
         try {
             String message = objectMapper.writeValueAsString(event);
 
+            // Pass correlationId as message attribute
+            // So downstream services can continue the same trace
+            String correlationId = MDC.get("correlationId");
+
             snsTemplate.send(orderEventsTopicArn,
                     MessageBuilder.withPayload(message)
                             .setHeader("eventType", "ORDER_CREATED")
+                            .setHeader("correlationId", correlationId)
                             .build());
 
             log.info("Published ORDER_CREATED event for orderId: {}", event.getOrderId());
